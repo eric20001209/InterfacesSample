e8e021bfe40fc30f4c5fe8488df65e655c2648dd
eric2000.c <eric2000.c@gmail.com>
2020-02-18 16:45:54 +1300

    Add project files.

---
+++ Polygons.Library/ConcreteRegularPolygon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygons.Library
+{
+	public class ConcreteRegularPolygon
+	{
+		public int NumberOfSides{ get; set; }
+		private int _sideLength;
+		public int SideLength
+		{
+			set { _sideLength = value; }
+			get { return _sideLength; }
+		}
+//		public int SideLength { get; set; }
+
+		public ConcreteRegularPolygon()
+		{
+			
+		}
+		public ConcreteRegularPolygon(int sides, int length)
+		{
+			NumberOfSides = sides;
+			SideLength = length;
+		}
+
+		public double GetPerimeter()
+		{
+			return NumberOfSides * SideLength;
+		}
+
+		public virtual double GetArea()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Polygons/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Polygons.Library;
+
+namespace Polygons
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Square square = new Square(10);
+			var area = square.GetArea();
+			Console.WriteLine($"Area is：\t{area}");
+		}
+	}
+}
