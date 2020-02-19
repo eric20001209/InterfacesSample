497d3b3ac29582af471a2ccb0c4f910002eae66a
eric2000.c <eric2000.c@gmail.com>
2020-02-19 16:50:24 +1300

    ..

---
+++ People.Library/PersonRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Common;
+
+namespace People.Library
+{
+	public class PersonRepository
+	{
+		public Person[] GetPeople() 
+		{
+			Person[] plist = new Person[9];
+			return plist;
+		}
+
+		public Person GetPerson(int id) 
+		{
+			return new Person();
+		}
+	}
+}
