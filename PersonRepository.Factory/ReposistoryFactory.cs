fd656e694778fc559f57dcf1cf8544b3b9abfa2a
eric2000.c <eric2000.c@gmail.com>
2020-02-24 09:29:31 +1300

    ..

---
+++ PersonRepository.Factory/ReposistoryFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonRepository.Interface;
+using System.Configuration;
+
+namespace PersonRepository.Factory
+{
+    public class ReposistoryFactory
+    {
+		public static IPersonRepository GetRepository()
+		{
+			string repositoryTypeName = ConfigurationManager.AppSettings["RepositoryType"];
+			Type reposityType = Type.GetType(repositoryTypeName);
+			object repository = Activator.CreateInstance(reposityType);
+			IPersonRepository personRepository = repository as IPersonRepository;
+			return personRepository;
+		}
+    }
+}
---
+++ PersonRepository.Interface/IPersonRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonRepository.Interface
+{
+	public interface IPersonRepository
+	{
+		void AddPerson(Person newPerson);  //Create
+		IEnumerable<Person> GetPeople();  //Read
+		Person GetPerson(int id);  //read
+		void UpdatePerson(int id, Person updatedPerson);
+		void DeletePerson(int id);
+	}
+}
